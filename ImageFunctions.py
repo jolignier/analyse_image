88258657c210a4085e286e965ac7161c2d8e3dca
Valentin <valentin.joly99@gmail.com>
2021-01-17 20:05:45 +0100

    Erosion / Dilatation

--- ImageFunctions.py
+++ ImageFunctions.py
@@ -45,7 +45,7 @@ def seuillage_bas(image: QImage, seuil: int) -> QImage:
     for i in range(0, image.width()):
         for j in range(0, image.height()):
             if image.pixel(i, j) < qRgb(seuil, seuil, seuil):
-                new_image.setPixel(i, j, qRgb(0,0,0))
+                new_image.setPixel(i, j, qRgb(0, 0, 0))
     return new_image
 
 
@@ -59,7 +59,7 @@ def addition(image1, image2) -> QImage:
             r = pixel1[0] + pixel2[0] if pixel1[0] + pixel2[0] < 255 else 255
             g = pixel1[1] + pixel2[1] if pixel1[1] + pixel2[1] < 255 else 255
             b = pixel1[2] + pixel2[2] if pixel1[2] + pixel2[2] < 255 else 255
-            new_image.setPixel(i, j, qRgb(r,g,b))
+            new_image.setPixel(i, j, qRgb(r, g, b))
     return new_image
 
 
@@ -77,14 +77,45 @@ def soustraction(image1: QImage, image2) -> QImage:
     return new_image
 
 
-def erosion(image, strel) -> QImage:
-    print("TODO")
-    return 0;
+def erosion(image: QImage, strel: list) -> QImage:
+    new_image = image
+
+    for i in range(len(strel) / 2, image.width - len(strel) / 2):
+        for j in range(len(strel) / 2, image.height - len(strel) / 2):
+
+
+
+            for k in range(i - len(strel) / 2, i + len(strel) / 2):
+                for l in range(j - len(strel) / 2, j + len(strel) / 2):
+
+                    if QColor(image.pixel(k, l)).getRgb()[0] == 0:
+                        new_image.setPixel(i, j, qRgb(0, 0, 0))
+                        break
+
+                if QColor(image.pixel(k, l)).getRgb()[0] == 0:
+                    break
+
+    return new_image;
 
 
 def dilatation(image, strel) -> QImage:
-    print("TODO")
-    return 0;
+    new_image = image
+
+    for i in range(len(strel) / 2, image.width - len(strel) / 2):
+        for j in range(len(strel) / 2, image.height - len(strel) / 2):
+
+
+            for k in range(i - len(strel) / 2, i + len(strel) / 2):
+                for l in range(j - len(strel) / 2, j + len(strel) / 2):
+
+                    if QColor(image.pixel(k, l)).getRgb()[0] == 255:
+                        new_image.setPixel(i, j, qRgb(255, 255, 255))
+                        break
+
+                if QColor(image.pixel(k, l)).getRgb()[0] == 255:
+                    break
+
+    return new_image;
 
 
 def ouverture(image, strel) -> QImage:
@@ -119,7 +150,7 @@ def squelettisation_amincissement_homothopique(image) -> QImage:
 
 def createStrel(dim, isBoule=False):
     strel = []
-    line =[255]*dim
+    line = [255] * dim
     for i in range(0, dim):
         strel.append(line)
 
